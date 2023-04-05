cc6ceedc0306e182aad5533226ae084af64243d3
Rodion Tytarenko <103597315+rodik5nizza@users.noreply.github.com>
2023-04-05 14:21:49 +0300

    Update index.js

--- store/src/index.js
+++ store/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { store } from './store'
 
@@ -10,10 +10,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 
-    <Provider store={store}>
-        <BrowserRouter>
+
+        <HashRouter>
+        <Provider store={store}>
             <App />
-        </BrowserRouter>
-    </Provider>
+        </Provider>
+        </HashRouter>
+
 );
 
